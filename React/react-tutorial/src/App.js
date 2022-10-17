d7d8cbf567c2e02270189e2d52fb836763b13061
sahuankit010 <sahu.ankit010@gmail.com>
2022-10-17 18:12:18 -0500

    react tutorial started

---
+++ React/react-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import Greet from './components/Greet'
+import Welcome from './components/Welcome';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet></Greet>
+      <Welcome></Welcome>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/react-tutorial/src/components/Greet.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Greet = () =>{
+    return <h1>Hello Ankit</h1>;
+}
+export default Greet
